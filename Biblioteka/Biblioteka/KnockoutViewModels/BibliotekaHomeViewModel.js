f1d7701fb2d3d54b034db54d622aa73ed3351258
biedro94 <biedrzyckid94@gmail.com>
2016-08-16 16:39:03 +0200

    asdasd

--- Biblioteka/Biblioteka/KnockoutViewModels/BibliotekaHomeViewModel.js
+++ Biblioteka/Biblioteka/KnockoutViewModels/BibliotekaHomeViewModel.js
@@ -2,19 +2,10 @@
     ko.applyBindings(new UsersViewModel());
 });
 
-//Create a user view-model class:
 function UsersViewModel() {
 
     var self = this;
     var baseUri = "http://localhost:53566/api/BibliotekaHome";
     self.users = ko.observableArray();
-
-    self.create = function (formElement) {
-        $.get(baseUri, $(formElement).serialize(), null, "json")
-            .done(function (o) {
-                self.users.push(o);
-            });
-    }
-    // Adds the value and notifies observers
     $.getJSON(baseUri, self.users);
 }
