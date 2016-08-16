0bdcdc800684ebbea33683776d5d5e6c31a7fd42
bkosmala <b.kosmala@interia.pl>
2016-08-16 14:10:48 +0200

    Nowy początek
    Nowy początek

---
+++ Biblioteka/Services/DataServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repositories;
+using Models;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+namespace Services
+{
+    public class DataServices 
+    {
+        public static List<string> GetListOfCategoriesServices()
+        {
+            return Repository.GetListOfCategories();
+        }
+
+    }
+}
