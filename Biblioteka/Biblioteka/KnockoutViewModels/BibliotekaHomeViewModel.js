a926fdba162005c115720371e840b3806667858a
bkosmala <b.kosmala@interia.pl>
2016-08-17 15:02:28 +0200

    s

--- Biblioteka/Biblioteka/KnockoutViewModels/BibliotekaHomeViewModel.js
+++ Biblioteka/Biblioteka/KnockoutViewModels/BibliotekaHomeViewModel.js
@@ -8,7 +8,7 @@ function CategoryNamesViewModel() {
     var getPozycjeUri = "http://localhost:53566/api/HomePozycje";
     self.category = ko.observableArray();
     self.pozycje = ko.observableArray();
-    
+
     $.getJSON(baseUri, self.category);
 
     self.SendCategoryName = function () {
@@ -21,33 +21,71 @@ function CategoryNamesViewModel() {
 
 
 
-    function Pozycja(idPoz, tytul, autor, opis, jezyk, wydawca, rokWydania, idKat) {
-        var self = this;
 
-        // observable are update elements upon changes, also update on element data changes [two way binding]
-        self.IdPoz = ko.observable(idPoz);
-        self.Tytul = ko.observable(tytul);
-        self.Autor = ko.observable(autor);
-        self.Opis = ko.observable(opis);
-        self.Jezyk = ko.observable(jezyk);
-        self.Wydawca = ko.observable(wydawca);
-        self.RokWydania = ko.observable(rokWydania);
-        self.IdKat = ko.observable(idKat);
-
-    };
 
 
     $.getJSON(getPozycjeUri, function (data) {
-        $.each(data, function (key, value) {
-            self.pozycje.push(new Pozycja(value.Id_poz, value.Tytul, value.Autor, value.Opis, value.Jezyk, value.Wydawca, value.Rok_wydania, value.Id_kategorii));
-        });
+        var a = data.map(x => new Pozycja(x))
+        self.pozycje(a);
     });
 
+    /*
+    var obj = [
+    { id: 1 },
+    { id: 2 }
+    ]
 
+    var result = ko.mapping.fromJS(obj, {
+        key: function (item) {
+            return ko.utils.unwrapObservable(item.id);
+        }
+    });*/
 
+    /*
+
+    $.getJSON(getPozycjeUri, function (data) {
+        pozycje = ko.mapping.fromJS(data, {
+            key: function (item) {
+                return new Pozycja(item);
+            }
+        });
+        ko.mapping.fromJS(data, pozycje);
+    });
+
+    */
     }
 
-   
+
+/*
+function Pozycja(idPoz, tytul, autor, opis, jezyk, wydawca, rokWydania, idKat) {
+    var self = this;
+
+    // observable are update elements upon changes, also update on element data changes [two way binding]
+    self.IdPoz = ko.observable(idPoz);
+    self.Tytul = ko.observable(tytul);
+    self.Autor = ko.observable(autor);
+    self.Opis = ko.observable(opis);
+    self.Jezyk = ko.observable(jezyk);
+    self.Wydawca = ko.observable(wydawca);
+    self.RokWydania = ko.observable(rokWydania);
+    self.IdKat = ko.observable(idKat);
+
+};
+*/
+function Pozycja(poz) {
+    var self = this;
+
+    // observable are update elements upon changes, also update on element data changes [two way binding]
+    self.IdPoz = ko.observable(poz.Id_poz);
+    self.Tytul = ko.observable(poz.Tytul);
+    self.Autor = ko.observable(poz.Autor);
+    self.Opis = ko.observable(poz.Opis);
+    self.Jezyk = ko.observable(poz.Jezyk);
+    self.Wydawca = ko.observable(poz.Wydawca);
+    self.RokWydania = ko.observable(poz.Rok_wydania);
+    self.IdKat = ko.observable(poz.Id_kategorii);
+
+};
 
     /*self.SendCategoryName = function(){
         var jsonData = ko.toJSON(self);
